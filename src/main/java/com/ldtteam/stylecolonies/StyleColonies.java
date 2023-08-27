655e61d71a7056529ff0c91c57e88b95b3054e78
Raycoms <ray.neiheiser@gmail.com>
2023-08-27 15:11:41 +0200

    Setup

---
+++ src/main/java/com/ldtteam/stylecolonies/StyleColonies.java
@@ -0,0 +1,12 @@
+package com.ldtteam.stylecolonies;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("stylecolonies")
+public class StyleColonies
+{
+    public StyleColonies()
+    {
+
+    }
+}

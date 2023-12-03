dae68fae0ca66baf2d3f99b4af1402cd59d1cf3e
Raycoms <ray.neiheiser@gmail.com>
2023-12-03 13:08:59 +0100

    WinterVillageStyle

---
+++ src/main/java/com/ldtteam/christmascolonies/ChristmasColonies.java
@@ -0,0 +1,12 @@
+package com.ldtteam.christmascolonies;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("christmascolonies")
+public class ChristmasColonies
+{
+    public ChristmasColonies()
+    {
+
+    }
+}
